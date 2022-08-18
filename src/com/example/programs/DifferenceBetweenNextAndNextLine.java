5bec6eeb46541fa6ba343cad15967c7e736e8b5d
asudheer09 <sudheer9.sr@gmail.com>
2022-08-18 21:56:46 +0530

    second commit

--- src/com/example/programs/DifferenceBetweenNextAndNextLine.java
+++ src/com/example/programs/DifferenceBetweenNextAndNextLine.java
@@ -1,4 +1,19 @@
 package com.example.programs;
 
+import java.util.Scanner;
+
 public class DifferenceBetweenNextAndNextLine {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        //next()
+        System.out.println("please enter a name");
+        String name=sc.next();
+        //String name=sc.nextLine();
+
+        System.out.println("you have entered name is "+name);
+
+    }
 }
---
+++ src/com/example/programs/LastIfExample.java
@@ -0,0 +1,22 @@
+package com.example.programs;
+
+public class LastIfExample {
+    //ctrl+y delete entire line
+
+    public static void main(String[] args) {
+        float avg=34;
+
+        if(avg>=75){ //80>=75 --true
+            System.out.println("distinction");
+        }else if(avg>=60 && avg<75){ //68>=60 && 68<75 --> true && true -->true , false && true--->false
+            System.out.println("first class");
+        }else if(avg>=45 && avg<60){ //true && true -->second class
+            System.out.println("second class");
+        }else if(avg>=35 && avg<45){
+            System.out.println("pass");
+        }else{
+            System.out.println("fail");
+        }
+
+    }
+}
